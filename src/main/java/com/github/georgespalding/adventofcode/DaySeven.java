94fc9b25e9643ca53d06cb82978059313304a44c
George Spalding <gspalding@invidi.se>
2018-12-07 22:33:41 +0100

    D7Pt1

--- src/main/java/com/github/georgespalding/adventofcode/DaySeven.java
+++ src/main/java/com/github/georgespalding/adventofcode/DaySeven.java
@@ -5,33 +5,32 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DaySeven {
 
-   static final Map<String, List<String>> rawDag =
-      Util.streamResource("7-s.lst")
+   private static final Map<String, List<String>> rawDag =
+      Util.streamResource("7.lst")
          .map(DaySeven::parse)
          .collect(groupingBy(s -> s.substring(0, 1)))
       .entrySet().stream()
          .collect(toMap(
             Entry::getKey,
             e -> e.getValue().stream()
-               //.peek(out::println)
                .map(s -> s.substring(1, 2))
                .sorted()
                .collect(toList())));
+
+   private static String parse(String line) {
+      //Step E must be finished before step B can begin.
+      return new String(new char[] { line.charAt(5), line.charAt(36) });
+   }
 
    public static void main(String[] args) {
       Map<String, Node> index = new HashMap<>();
@@ -43,42 +42,19 @@ public class DaySeven {
             dNode.getUpStream().add(node);
          });
       });
-      Set<Node> downstreams = new HashSet<>(index.values());
-      List<String> rootsInLayerOrder=new ArrayList<>();
-      while(!downstreams.isEmpty()){
-         final Set<Node> prevDownstream = downstreams;
-         downstreams=downstreams(downstreams);
-         final List<Node> roots = roots(prevDownstream, downstreams);
-         final String sroots = roots.stream().map(Node::getName).collect(joining());
-         rootsInLayerOrder.add(sroots);
-         out.print(sroots);
-         out.print(",");
-      }
-      out.println();
-
-      out.println(
-         rootsInLayerOrder.stream().mapToInt(String::length).sum()
-      +": "+ String.join("", rootsInLayerOrder));
-      out.println(index.values().size());
-   }
-
-   private static Set<Node> downstreams(Collection<Node> nodes) {
-      return nodes.stream()
-         .map(Node::getDownStream)
-         .flatMap(Collection::stream)
-         .collect(toSet());
-   }
-
-   private static List<Node> roots(Collection<Node> nodes, Collection<Node> downstreams) {
-      return nodes.stream()
-         .filter(k -> !downstreams.contains(k))
-         .sorted()
-         .collect(toList());
-   }
-
-   private static String parse(String line) {
-      //Step E must be finished before step B can begin.
-      return new String(new char[] { line.charAt(5), line.charAt(36) });
+      final List<Node> remaining = index.values().stream().sorted().collect(toList());
+      final List<Node> done = new ArrayList<>();
+      do {
+         remaining.stream()
+            .filter(n -> n.notBlocked(done)).sorted()
+            .findFirst()
+            .ifPresent(firstUnBlocked -> {
+               done.add(firstUnBlocked);
+               remaining.remove(firstUnBlocked);
+            });
+      } while (!remaining.isEmpty());
+      out.println(done);
+      out.println(done.stream().map(Node::getName).collect(joining()));
    }
 
    static class Node implements Comparable<Node>{
@@ -91,16 +67,20 @@ public class DaySeven {
          this.name = name;
       }
 
-      public String getName() {
+      String getName() {
          return name;
       }
 
-      public List<Node> getDownStream() {
+      List<Node> getDownStream() {
          return downStream;
       }
 
-      public List<Node> getUpStream() {
+      List<Node> getUpStream() {
          return upStream;
+      }
+
+      boolean notBlocked(List<Node> done) {
+         return done.containsAll(upStream);
       }
 
       @Override
@@ -120,15 +100,9 @@ public class DaySeven {
          return name.equals(node.name);
       }
 
-      public String desc() {
-         return "Node: "
-            + name
-            + " -> " + downStream.stream().map(Object::toString).collect(joining(", "));
-      }
-
       @Override
       public String toString() {
-         return "Node{"+ name + '}';
+         return "{" + name + ": " + downStream.stream().map(Node::getName).collect(joining()) + "}";
       }
 
       @Override
