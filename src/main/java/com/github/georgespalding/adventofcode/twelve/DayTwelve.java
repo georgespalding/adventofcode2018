c8cd60eb5975554504fa6e565426f3c766460baf
George Spalding <gspalding@ooyala.com>
2018-12-13 08:25:51 +0100

    Worst bug ever
    Tries to get the correct results using the sample data.

--- src/main/java/com/github/georgespalding/adventofcode/twelve/DayTwelve.java
+++ src/main/java/com/github/georgespalding/adventofcode/twelve/DayTwelve.java
@@ -4,40 +4,47 @@ import static com.github.georgespalding.adventofcode.Util.streamResource;
 import static java.lang.System.out;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
+import static java.util.stream.IntStream.rangeClosed;
 
 import com.github.georgespalding.adventofcode.template.Day;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class DayTwelve {
 
-   static final boolean debug = false;
+   static final boolean debug = true;
 
    public static void main(String[] args) {
-      final Day<Long, String> dayNine = new Day<>();
-      final int iterations = 20;
+      final Day<Long, BigInteger> dayTwelve = new Day<>();
       final List<String> lines = streamResource("12.lst").collect(toList());
       final BinaryThing thing = new BinaryThing(
          lines.stream()
             .filter(s -> s.startsWith("initial state: "))
             .findFirst()
             .orElseThrow(() -> new RuntimeException("initial state not found")),
-         iterations + 2,
          lines.stream().filter(l -> l.contains(" => ")));
 
-      dayNine.start();
+      dayTwelve.start();
       if (debug) out.println(thing.header());
 
       if (debug) out.printf("%2d: %s\n", 0, thing.toString());
-      range(0, iterations)
+      rangeClosed(1, 20)
+         .forEach(i -> {
+            thing.generation();
+            if (debug) out.printf("%2d: %s\n", i, thing.toString());
+         });
+      dayTwelve.partOne(thing.potNumSum());
+
+      LongStream.rangeClosed(21L, 100)
          .forEach(i -> {
             thing.generation();
             if (debug) out.printf("%2d: %s\n", i, thing.toString());
          });
 
-      dayNine.partOne(thing.potNumSum());
-      dayNine.partTwo("TODO Part2");
+      dayTwelve.partTwo(thing.hyperJumpAt100());
 
-      dayNine.output();
+      dayTwelve.output();
    }
 }
