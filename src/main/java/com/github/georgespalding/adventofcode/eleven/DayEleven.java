84f81c1db9f951bad0737c84955ee240ecc7d5d5
George Spalding <gspalding@invidi.se>
2018-12-13 13:07:36 +0100

    DayEleven...

---
+++ src/main/java/com/github/georgespalding/adventofcode/eleven/DayEleven.java
@@ -0,0 +1,31 @@
+package com.github.georgespalding.adventofcode.eleven;
+
+import static java.lang.System.out;
+
+import com.github.georgespalding.adventofcode.template.Day;
+
+import java.util.stream.IntStream;
+
+public class DayEleven {
+
+   static final boolean debug = true;
+
+   public static void main(String[] args) {
+      final Day<Object, Object> dayNine = new Day<>();
+      if(debug) {
+         out.println(new Grid(8, 300, 300).getCell(3, 5));
+         out.println(new Grid(57, 300, 300).getCell(122, 79));
+         out.println(new Grid(39, 300, 300).getCell(217, 196));
+         out.println(new Grid(71, 300, 300).getCell(101, 153));
+         out.println(new Grid(18, 300, 300).findBestSquare(3));
+         out.println(new Grid(42, 300, 300).findBestSquare(3));
+      }
+      Grid part1=new Grid(8772,300,300);
+      dayNine.start();
+
+      dayNine.partOne(part1.findBestSquare(3));
+      dayNine.partTwo(part1.findBestSquare());
+
+      dayNine.output();
+   }
+}
---
+++ src/main/java/com/github/georgespalding/adventofcode/eleven/Grid.java
@@ -0,0 +1,72 @@
+package com.github.georgespalding.adventofcode.eleven;
+
+import static com.github.georgespalding.adventofcode.Pair.fromEntry;
+import static java.util.Comparator.comparingLong;
+import static java.util.stream.LongStream.rangeClosed;
+
+import com.github.georgespalding.adventofcode.Pair;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Grid {
+
+   final int gridSerialNumber;
+   final long[][] fuelCells;
+
+   public Grid(int gridSerialNumber, int xDim, int yDim) {
+      this.gridSerialNumber = gridSerialNumber;
+      this.fuelCells = new long[xDim][yDim];
+      IntStream.rangeClosed(1, yDim).forEach(y ->
+         IntStream.rangeClosed(1, xDim).forEach(x ->
+            fuelCells[x - 1][y - 1] = cellPower(gridSerialNumber, x, y)));
+   }
+
+   static long cellPower(int gridSerialNumber, int x, int y) {
+      // Find the fuel cell's rack ID, which is its X coordinate plus 10.
+      final long rackId = x + 10;
+      return (((
+                  // Begin with a power level of the rack ID times the Y coordinate.
+                  rackId * y
+                     // Increase the power level by the value of the grid serial number (your puzzle input).
+                     + gridSerialNumber)
+                  // Set the power level to itself multiplied by the rack ID.
+                  * rackId)
+                 //Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+                 % 1000) / 100
+         // Subtract 5 from the power level.
+         - 5;
+   }
+
+   long powerOfSquare(int x, int y, int dim) {
+      return rangeClosed(y, y + dim - 1)
+         .flatMap(yG ->
+            rangeClosed(x, x + dim - 1)
+               .map(xG ->
+                  getCell((int) xG, (int) yG)))
+         .sum();
+   }
+
+   Optional<Pair<String, Long>> findBestSquare() {
+      return IntStream.rangeClosed(1,fuelCells.length)
+         .peek(dim-> System.out.println("Dim:"+dim+" "+System.currentTimeMillis()))
+         .mapToObj(this::findBestSquare)
+         .filter(Optional::isPresent)
+         .map(Optional::get)
+         .max(comparingLong(Pair::getVal));
+
+   }
+   Optional<Pair<String, Long>> findBestSquare(int dim) {
+      return IntStream.rangeClosed(1, fuelCells.length - (dim-1))
+         .mapToObj(y -> IntStream.rangeClosed(1, fuelCells.length - (dim-1))
+            .mapToObj(x -> fromEntry(dim+": "+x + "," + y, powerOfSquare(x, y, dim))))
+         .flatMap(s -> s)
+         .max(comparingLong(Pair::getVal));
+   }
+
+   long getCell(int x, int y) {
+      return fuelCells[x - 1][y - 1];
+   }
+
+}
